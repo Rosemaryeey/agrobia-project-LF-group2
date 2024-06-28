bef000d9863c97509ac04870fba3ce4e33786c1f
Junior's signature <chibuzor.john.2018@gmail.com>
2024-06-28 07:53:35 +0100

    added signup and login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from 'react';
+import './login.css';
+import amico from '../assets/amico.png';
 
 function login() {
   return (
-    <div>login</div>
-  )
-}
+    <div>
+      <div class="login-form">
+        
+        <img src={amico} alt=""/>
+    <form action="" method="POST">
+            
+            <div class="form-div">
+                    
+                            <div class="title">
+                                <h3>
+                                    Login
+                                </h3>
+                            </div>
+                            <br/>
+                            <div class="google-auth login">
+                                <a  href=''>
+                                    <i class="bi bi-google"></i>
+                                    &nbsp;
+                                    <b>Login with Google</b>
+                                </a> 
+                            </div>
+                            <br/><br/>       
+                    <input type="text" class="form-control" name="username" placeholder="Enter your email address..."/>                       
+            
+                <input type="password" class="form-control" name="password" placeholder="Enter your password..."/>
+                
+                <div class="t_c">
+                    <input type="checkbox" name="" id="" checked />
+                    <small>
+                        Remember me.
+                    </small>
+                </div>
+                    <button type="submit" class="form-btn">
+                        Login
+                    </button>
+                    <br/>
+                    <br/>
+                    <center>
+                        <small>
+                            <a href="">
+                                Don't Have an Account?
+                            </a>
+                        </small>
+                    </center>
+                </div>
+    </form>
+  </div>
+    </div>
+  );
+};
 
-export default login
+export default login;
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,80 +1,46 @@
-// import React from 'react'
-// import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom';
 
 
-// function signup() {
-//   return (
-//     <div className=" h-screen w-screen">
-//       <section
-//         className=" h-screen w-screen  bg-[linear-gradient(to_bottom,rgba(0,0,0.2),rgba(0,0,0,0.2)),url(./assets/agro.jpeg)] bg-cover bg-no-repeat bg-center
-//        flex items-center justify-center"
-//       >
-//         <form className="w-[35vw] h-[75vh] bg-white/15   items-center flex p-5 justify-center flex-col">
-//           <h2 className="flex items-center text-5xl font-bold font-mono text-white">
-//             {" "}
-//             <span
-//               className=" text-5xl font-bold font-mono
-//              bg-clip-text text-transparent bg-gradient-to-r from-white to-green-700"
-//             >
-//               Sign
-//             </span>
-//             up
-//           </h2>
+function signup() {
+  return (
+    <div className=" h-screen w-screen">
+     
+     <div class="register-container">
+                <h3>
+                    Create an account
+                </h3>
+                <br/>
+                <a class="google-auth" href=''>
+                    <i class="bi bi-google"></i>
+                    &nbsp;
+                    <b>Sign up with Google</b>
+                </a>
+                <form action="" method="POST">
 
-//           <section className=" w-[25vw] h-[60vh] flex items-start justify-around flex-col">
-//             <div className="">
-//               <label className="text-green-500">Name:</label>
-//               <input
-//                 className=" py-2 px-7  w-[23vw] bg-transparent"
-//                 type="text"
-//                 placeholder="Enter name"
-//               />
-//             </div>
-//             <div>
-//               <label className="text-green-500">Email:</label>
-//               <input
-//                 className=" py-2 px-7  w-[23vw]  bg-transparent"
-//                 type="email"
-//                 placeholder="Enter Email"
-//               />
-//             </div>
-//             <div>
-//               <label className="text-green-500">Location:</label>
-//               <input
-//                 placeholder="Farm location"
-//                 className=" py-2 px-7  w-[23vw]  bg-transparent"
-//                 type="search"
-//                 name="location"
-//                 id="location"
-//               />
-//             </div>
+                    <div class="form-div">
+                        <input type="text" placeholder='User Name'/>
+                        <input type="text" placeholder="Phone Number" />
+                        <input type="email" placeholder='Email address' />
+                        <input type="password" placeholder='Password' />
+                        <input type="password" placeholder='Confirm password' />
+                    <div class="t_c">
+                        <input type="checkbox" name="" id="" required/>
+                        <small>
+                            I agree with the terms and services and all policy statements.
+                        </small>
+                    </div>
+                    <button type="submit" class='form-btn'><b>
+                        Sign Up
+                    </b></button>
+                    </div>
+                    <br/>
+                    
+                </form>
+            </div>
+            
+    </div>
+  );
+}
 
-//             <div>
-//               <label className="text-green-500">Farm Produce:</label>
-//               <select
-//                 className=" py-2 px-7  w-[23vw]  bg-transparent"
-//                 name=" produce"
-//                 id="produce"
-//               >
-//                 <option value=""></option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//                 <option value="">corn</option>
-//               </select>
-//             </div>
-
-//           </section>
-//         </form>
-//       </section>
-//     </div>
-//   );
-// }
-
-// export default signup
+export default signup;
